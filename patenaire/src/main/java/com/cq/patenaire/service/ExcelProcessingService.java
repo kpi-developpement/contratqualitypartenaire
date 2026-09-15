a48c6b7f8757a8e5e1f74dc1c2f0cf93a9812236
kpi-developpement <kpi-developpement>
2026-09-15 11:14:12 +0100

    add satcli ok and nok 2

--- patenaire/src/main/java/com/cq/patenaire/service/ExcelProcessingService.java
+++ patenaire/src/main/java/com/cq/patenaire/service/ExcelProcessingService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
